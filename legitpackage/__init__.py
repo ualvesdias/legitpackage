eaac2ae5e679c4ec6e1249b314ef2359e914ba43
Ulisses Alves <ulisses.alves@protonmail.com>
2021-02-11 22:50:35 -0300

    This is the first commit.

---
+++ legitpackage/__init__.py
@@ -0,0 +1,2 @@
+from legitpackage.file1 import function1, function2
+from legitpackage.file2 import function1, function2
---
+++ legitpackage/file1.py
@@ -0,0 +1,6 @@
+def function1():
+    print('This is function 1 from file 1')
+
+def function2():
+    print('This is function 2 from file 1')
+
